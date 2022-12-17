aed48169c9cfc079e96b1a2e074f6ebb143db143
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-17 12:59:54 +0530

    working-with-append-file-with-fs-module

--- fsModule/demo.js
+++ fsModule/demo.js
@@ -9,10 +9,18 @@ fs.readdir('c:/',function(err,data){
     console.log(data);
 });
 
-fs.writeFile('fsfile.txt','working with fs.write asynchronously',function(err,data){
+// fs.writeFile('fsfile.txt','working with fs.write asynchronously',function(err,data){
+//     if(err){
+//         console.log('error occured',er);
+//     }else{
+//         console.log('message saved!');
+//     }
+// })
+
+fs.appendFile('fsfile.txt','working with append-file asynchronously with the help of fs module',function(err){
     if(err){
-        console.log('error occured',er);
-    }else{
-        console.log('message saved!');
+        console.log(err);
     }
+    console.log('message appended');
 })
+
