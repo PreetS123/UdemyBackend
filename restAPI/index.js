30b31077abb4e763fcdacac772cc6977191338c6
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-18 21:29:17 +0530

    rest-api

--- restAPI/index.js
+++ restAPI/index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app= express();// express() function creates an instance express application object with methods of routing http request. It is called app by tradition.
-const joi= require('@hapi/joi');// joi is used for validation.
+
+// joi is used for validation.
 const Joi = require('@hapi/joi');
 app.use(express.json());// it is inbuilt method in express to recognize the incoming request object as a json object.
                         // This method is called as a middleware.
