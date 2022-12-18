efdade0bf69650eeeaa6672c94f515079c6c5306
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-18 21:18:08 +0530

    put-delete-not-working

--- restAPI/index.js
+++ restAPI/index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app= express();// express() function creates an instance express application object with methods of routing http request. It is called app by tradition.
 const joi= require('@hapi/joi');// joi is used for validation.
+const Joi = require('@hapi/joi');
 app.use(express.json());// it is inbuilt method in express to recognize the incoming request object as a json object.
                         // This method is called as a middleware.
 
@@ -45,6 +46,52 @@ app.get('/api/fruits/:id',(req,res)=>{
     }
 })
 
+
+app.post('/api/fruits',(req,res)=>{
+    const {error}= validateFruit(req.body);
+    if(error){
+        return res.status(400).send(error.details[0].message)
+    }
+    const fruit={
+        id:fruits.length+1,
+        name:req.body.name
+    };
+    fruits.push(fruit);
+    res.send(fruit);
+})
+
+
+app.put('/api/fruits/:id',(req,res)=>{
+    const fruit= fruits.find(c=>c.id===parseInt(req.params.id));
+    if(!fruit){
+        return res.status(404).setDefaultEncoding('Fruit Not Found!');
+     }
+        const {error}= validateFruit(req.body);
+        if(error){
+                return res.status(400).send(error.details[0].message);
+        }
+        fruit.name= req.body.name;
+        return res.send(fruit);
+    
+})
+
+app.delete('/api/fruits/:id',(req,res)=>{
+    const fruit= fruits.find(c=>c.id===parseInt(req.params.id));
+    if(!fruit){
+        return res.status(404).send('fruit not found');
+    }
+    const index=fruits.indexOf(fruit);
+      fruits.splice(index,1);
+      res.send(fruit);
+})
+
+function validateFruit(fruit){
+    const schema=Joi.object({
+        name:Joi.string().min(3).required()
+    });
+    const validation=schema.validate(fruit);
+    return validation;
+}
 
 const port=process.env.PORT||5001;
 app.listen(port,()=>{
