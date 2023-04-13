515a818f3fdf278f0be810f5b32b730247fde31e
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-13 22:09:35 +0530

    password handlening

--- AdvanceBackend/backend-authentication/app.js
+++ AdvanceBackend/backend-authentication/app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 require('dotenv').config();
-const cors=require('cors');
+const bcrypt= require('bcryptjs');
+const jwt= require('jsonwebtoken');
 const User= require('./model/User.model');
 
 const app= express();
@@ -11,14 +12,42 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/register',async (req,res)=>{
-    const {firstname,lastname,email,password}= req.body;
-    if(!(email&& password && firstname && lastname)){
-        res.status(400).send('All fiels are required');
-    }
-    const existingUser= await User.findOne({email});
-    if(existingUser){
-        res.status(401).send('User already exist');
-    }
+    
+        try{
+            const {firstname,lastname,email,password}= req.body;
+            if(!(email&& password && firstname && lastname)){
+                res.status(400).send('All fiels are required');
+            }
+            const existingUser= await User.findOne({email});
+            if(existingUser){
+                res.status(401).send('User already exist');
+            }
+            let myEncPassword= await bcrypt.hash(password,10)
+           const user= await  User.create({
+                firstname,
+                lastname,
+                email,
+                password:myEncPassword,
+            })
+            //token creation
+            const token=jwt.sign(
+                {user_id:user._id,email},
+                 process.env.SECRET_KEY,
+                 {
+                    expiresIn:'4h'
+                 }
+                )
+                user.token=token
+                //update or not in DB
+                
+                //password handlenling
+                user.password=undefined;
+
+                res.status(201).json(user)
+        }
+        catch(error){
+           console.log(error)
+        }
 })
 
 
